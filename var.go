04ec72775816d08b46f84631ed50885e8f31f872
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-03-25 16:04:13 +0000

    go intro and variables

---
+++ var.go
@@ -0,0 +1,12 @@
+
+// we declare vriables by using var keyword then name finally the dattype
+// var variable_name type = value
+// variable_name := value
+package main
+import "fmt"
+func main(){
+	var name string = "koech"
+
+fmt.Println(name)
+
+}
