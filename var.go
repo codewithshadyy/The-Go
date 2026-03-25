bfc10979f9a85853c5b858b391739c029af66304
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-03-25 16:33:28 +0000

    learning branching using this file

--- var.go
+++ var.go
@@ -2,6 +2,7 @@
 // we declare vriables by using var keyword then name finally the dattype
 // var variable_name type = value
 // variable_name := value
+// varibles can be declared in a single line
 package main
 import "fmt"
 func main(){
