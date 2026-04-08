906a2164f9478960dd7901cd19fb2ae5e0ca7ca8
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-04-09 02:31:25 +0300

    The Go data types:

---
+++ Go_data_types/data_types.go
@@ -0,0 +1,26 @@
+
+
+// go is comprise of four data types :
+
+
+// (
+// 	integer- sign = (positive and negative) - unsigned = (positive)
+// 	string - "superdad"
+// 	bool = true, false
+// 	float = 567.90
+// )
+
+package main
+
+import "fmt"
+
+func main(){
+	var age int = 78
+	var isTrue bool  = true
+	var answer string  = "yes it's "
+
+	fmt.Println(answer,   isTrue,  " " + " he is ", age)
+}
+
+
+
